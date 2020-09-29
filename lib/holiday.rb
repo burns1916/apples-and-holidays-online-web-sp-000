b2d4952746bbe362fc822247dcc26c2654e2ba6d
Ryan K Burns <ryankburns@gmail.com>
2020-09-29 16:54:12 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -51,12 +51,12 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   # code here
 holiday_hash.each do |time_of_year, value|
   if time_of_year == season
-    time_of_year.each_key(holiday_name)
-
+    holiday_hash[:season] = holiday_name
+    break
   # remember to return the updated hash
 end
 end
-return holiday_hash
+holiday_hash
 end
 
 def all_winter_holiday_supplies(holiday_hash)
