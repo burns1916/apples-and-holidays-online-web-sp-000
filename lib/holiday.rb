6e321106e92fc5ee2c200f03a6b06ee2a23f14a1
Ryan K Burns <ryankburns@gmail.com>
2020-09-29 17:03:17 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,6 +60,7 @@ def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
 holiday_hash.each do |season, holiday|
   if season == :winter
+    binding.pry
     holiday.each_value {|supplies| puts supplies}
   end
 end
