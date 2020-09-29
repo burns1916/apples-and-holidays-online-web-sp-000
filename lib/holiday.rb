ce61b5987c1ba1f5ca1cf35dcde691f5ea922ec8
Ryan K Burns <ryankburns@gmail.com>
2020-09-29 17:06:02 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,6 +61,7 @@ def all_winter_holiday_supplies(holiday_hash)
 holiday_hash.each do |season, holiday|
   if season == :winter
     holiday.each {|supplies| puts "#{supplies}"}
+    binding.pry
   end
 end
 end
